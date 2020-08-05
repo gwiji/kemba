c72025a25b7520f7ceed66c40b371843c1906158
gwijie <me@gwijie.com>
2020-08-05 12:33:28 +0300

    debug

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ const myPeer = new Peer(undefined, {
 })
 
 
-const myVideo = document.createElement('video')
+const myVideo = document.createElement('video');
 myVideo.muted = true;
 const peers = {}
 
