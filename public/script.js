f49654fb08f37dbcf4db57cc35f10cc9c149e193
gwijie <me@gwijie.com>
2020-08-05 13:25:00 +0300

    removed tag

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer(undefined, {
-  host: 'https://kemba.herokuapp.com/',
+  host: 'kemba.herokuapp.com/',
   secure:true, 
   port:443
 })
