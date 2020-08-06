5015565913bf76f15fea57d49e61672f9222bdcf
gwijie <me@gwijie.com>
2020-08-06 13:12:32 +0300

    debug

--- public/script.js
+++ public/script.js
@@ -76,7 +76,7 @@ function connectToNewUser(userId, stream) {
         call.close();
         socket.disconnect();
         $(this).css({display: "none"});
-        location.reload();
+        //location.reload();
 
         userVideoStream.getTracks().forEach(function(track) {
             if (track.readyState == 'live') {
