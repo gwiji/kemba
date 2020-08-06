5c2545664207a144e49ce5493413c75b3a814b70
gwijie <me@gwijie.com>
2020-08-06 13:21:11 +0300

    debug

--- public/script.js
+++ public/script.js
@@ -11,44 +11,53 @@ const myVideo = document.createElement('video');
 myVideo.muted = true;
 const peers = {}
 
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
+$('#strim').click(
+    function(){
+    strim()
+    $(this).css({display: "none"})
+    }
+);
 
-  addVideoStream(myVideo, stream)
-
-  myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
-
-    call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream)
-        console.log('Peer on stream');
-    })
-  })
-
-  socket.on('user-connected', userId => {
-    
-    console.log('peers connected',peers)
-    $('#contacts').html('<br><button class="call btn btn-primary text-center">Share Video</button>');
-    $('.call').click(function(){
-        socket.emit('disconnect')
-        $(this).css({display: 'none'});
-        connectToNewUser(userId, stream)
-        $('#contacts').html('<br><button class="btn btn-danger" id="disconnect">End Call</button>');
-    })
-    
-  })
-
-  socket.on('user-disconnected', userId => {
-    console.log('peers disconnected',userId);
-    //myPeers.close() 
-    console.log('disconnected',userId);
-    
-  })
-
-})
+function strim(){
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+      }).then(stream => {
+      
+        addVideoStream(myVideo, stream)
+      
+        myPeer.on('call', call => {
+          call.answer(stream)
+          const video = document.createElement('video')
+      
+          call.on('stream', userVideoStream => {
+              addVideoStream(video, userVideoStream)
+              console.log('Peer on stream');
+          })
+        })
+      
+        socket.on('user-connected', userId => {
+          
+          console.log('peers connected',peers)
+          $('#contacts').html('<br><button class="call btn btn-primary text-center">Share Video</button>');
+          $('.call').click(function(){
+              socket.emit('disconnect')
+              $(this).css({display: 'none'});
+              connectToNewUser(userId, stream)
+              $('#contacts').html('<br><button class="btn btn-danger" id="disconnect">End Call</button>');
+          })
+          
+        })
+      
+        socket.on('user-disconnected', userId => {
+          console.log('peers disconnected',userId);
+          //myPeers.close() 
+          console.log('disconnected',userId);
+          
+        })
+      
+      })
+}
 /*
 socket.on('user-disconnected', userId => {
     console.log('peers',peers)
@@ -76,12 +85,10 @@ function connectToNewUser(userId, stream) {
         call.close();
         socket.disconnect();
         $(this).css({display: "none"});
-        //location.reload();
 
-        userVideoStream.getTracks().forEach(function(track) {
+        stream.getTracks().forEach(function(track) {
             if (track.readyState == 'live') {
                 track.stop();
-                console.log(track.readyState)
             }
         });
 
