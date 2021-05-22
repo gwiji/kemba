5972363376889923c4476c471f3925cfecb8d0ee
gwijie <me@gwijie.com>
2021-05-22 10:59:05 +0300

    further updates

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer({
-  host: 'https://maroon-tyrannosaurus-jk4vo8r5.ws-eu07.gitpod.io',
+  host: 'https://9000-maroon-tyrannosaurus-jk4vo8r5.ws-eu07.gitpod.io',
   secure: true, 
   port: 443
 })
